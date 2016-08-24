5b0657fbe4ae9187b9c36a3d3fd204d9349ed86b
chenyanxu <yanxu2016>
2016-08-24 08:21:56 +0800

    1.remove the oa-core-web 2.change the package type of oa-core-etc to bundle 3.config the dict service to auto update when the dict table changed

--- system-parent/dict-parent/oa-system-dict-api/src/main/java/com/kalix/oa/system/dict/api/biz/IOADictBeanService.java
+++ system-parent/dict-parent/oa-system-dict-api/src/main/java/com/kalix/oa/system/dict/api/biz/IOADictBeanService.java
@@ -1,11 +1,7 @@
 package com.kalix.oa.system.dict.api.biz;
 
-import com.kalix.framework.core.api.biz.IBizService;
+import com.kalix.framework.core.api.system.IDictBeanService;
 import com.kalix.oa.system.dict.entities.OADictBean;
 
-import java.util.List;
-import java.util.Map;
-
-public interface IOADictBeanService extends IBizService<OADictBean> {
-    List<Map> getDictTypes(String query);
+public interface IOADictBeanService extends IDictBeanService<OADictBean> {
 }
